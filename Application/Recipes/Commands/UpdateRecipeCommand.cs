ce5e019c69ae02f788fb975d8de604fe094b2e2d
Sean Rodriguez <sean.rodriguez@outlook.com>
2022-01-27 21:39:25 -0500

    Update command to update ingredient list as well as the recipe data

--- Application/Recipes/Commands/UpdateRecipeCommand.cs
+++ Application/Recipes/Commands/UpdateRecipeCommand.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Interfaces;
+using Application.Recipes.Queries;
 using AutoMapper;
 using Domain.Entities;
 using MediatR;
@@ -9,6 +11,7 @@ namespace Application.Recipes.Commands {
     public class UpdateRecipeCommand : IRequest<Unit> {
         public int Id { get; set; }
         public string Name {get; set;}
+        public IEnumerable<RecipeIngredientResponse> Ingredients { get; set; }
 
         public class UpdateRecipeCommandHandler : IRequestHandler<UpdateRecipeCommand, Unit>
         {
@@ -24,7 +27,7 @@ namespace Application.Recipes.Commands {
             public async Task<Unit> Handle(UpdateRecipeCommand request, CancellationToken cancellationToken)
             {
                 Recipe recipe = _mapper.Map<Recipe>(request);
-                await _repository.UpdateAsync(recipe);
+                await _repository.UpdateRecipe(recipe);
                 return Unit.Value;
             }
         }
